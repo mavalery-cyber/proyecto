6352709870d0ee336499d36bd17a27da8f7e85aa
mavalery <74892046+mavalery-cyber@users.noreply.github.com>
2021-03-12 00:47:56 -0600

    Update album.model.js

--- models/album.model.js
+++ models/album.model.js
@@ -6,6 +6,7 @@ const schema_album = mongoose.Schema({
     codigo: { type: String, required: true, unique: true },
     nombre: { type: String, required: true },
     fecha_lanza: { type: String, required: true },
+    cant_canciones: { type: Number, required: true },
 });
 
 module.exports = mongoose.model('Album', schema_album, 'albumes');
