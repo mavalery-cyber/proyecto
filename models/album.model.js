448de6b000279c79d651688cff267003631472f4
mavalery <74892046+mavalery-cyber@users.noreply.github.com>
2021-03-12 00:44:46 -0600

    Update album.model.js

--- models/album.model.js
+++ models/album.model.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 
-const schema_artista = mongoose.Schema({
+const schema_album = mongoose.Schema({
     codigo: { type: String, required: true, unique: true },
     nombre: { type: String, required: true },
     fecha_lanza: { type: String, required: true },
